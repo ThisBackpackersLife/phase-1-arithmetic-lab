50db0fc9978e485612772813038539cdc376e787
Nicholas <crazedglitch@yahoo.com>
2023-03-18 12:48:10 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const num1 = 2;
-const num2 = 31;
+const num1 = 31
+const num2 = 2
 
-const  multiply = num1 * num2;
+const multiply = num1 * num2;
 
-const random = Math.floor(Math.random() * 10) +1;
+const random = Math.floor(Math.random() * 10) + 1;
 
-const num3 = 32;
-const num4 = 4;
+const num3 = 12
+const num4 = 4
 
 const mod = num4 % num3;
 
-const max = Math.max(1, 2, 4, 10, 20)
-// Write your code here
+const max = Math.max(1, 3, 5, 15, 20);
